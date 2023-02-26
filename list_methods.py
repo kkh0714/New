da7bc2f85080a7a11f346027fa0051e371870490
Keith Ching <keith@192-168-1-109.tpgi.com.au>
2023-02-26 14:21:55 +1100

    learnt listing methods

---
+++ list_methods.py
@@ -0,0 +1,24 @@
+numbers = [5, 2, 1, 5, 7, 4]
+numbers2 = numbers.copy()
+numbers.append(10)
+
+# numbers.append(20)
+
+# numbers.insert(0, 10)
+
+# numbers.remove(5) #remove() only remove the first object if there are more than one same object
+
+# numbers.clear() #clear() doesn't take any value
+
+# numbers.pop()
+
+# print(numbers.index(7)) #index() checks the index of an object 
+
+# print(50 in numbers) #check whether an obejct is inside a list
+
+# print(numbers.count(5))
+
+# numbers.sort()
+# numbers.reverse()
+
+print(numbers2)
