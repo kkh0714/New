973a1951aa26de667d63b917581a8adfbee5f191
Keith Ching <keith@192-168-1-109.tpgi.com.au>
2023-02-26 13:54:46 +1100

    learnt about list inside a list

---
+++ matrix.py
@@ -0,0 +1,9 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# we are iterating through matrix list. In each iteration, row will contain 1 list/item
+for row in matrix:
+    for item in row:
+        print(item)
